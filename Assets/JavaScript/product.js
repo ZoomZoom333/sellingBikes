b3d5f332b3ca352a3857491f1f7149bb65d17f2c
ZoomZoom333 <beautifulworldzumi@gmail.com>
2015-11-12 14:42:06 -0500

    changes

--- Assets/JavaScript/product.js
+++ Assets/JavaScript/product.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-	$(".library").hover(function() {
-		$("oneMount"):has('img').css({ width: '300', height: '315px' });
+	$("img.magnify").mouseenter(function() {
+	 $(this).css({ width: '300', height: '315px' });
 	});
+	$("img.magnify").mouseleave(function() {
+		$(this).css({width: '200', height: '200'});
+	});
+	$("a.mountain").click(function() {
+		$("div").find("#oneMount").show('img');
+	});
+	$("a.riding").click(function() {
+		$("#twoRdidng.library").show();
+	});
+	$("a.bmx").click(function() {
+		$("#threeBmx").show();
+	});
+	$("a.beach").click(function() {
+		$("#fourBeachcruiser").show();
+	});
+	/*document.getElementById("mountain").onclick = function() {
+		$("div").find("#oneMount").show();
+	};*/
 });
