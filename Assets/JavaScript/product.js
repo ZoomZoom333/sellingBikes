7591cdfd1ecaa867fd224d6e8bc01830994a7b21
ZoomZoom333 <beautifulworldzumi@gmail.com>
2015-11-13 14:33:32 -0500

    I figured out the jquery for the gallery

--- Assets/JavaScript/product.js
+++ Assets/JavaScript/product.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
+	$("a.mountain").click(function() {
+		$("#oneMount").toggle();
+	});
+	$("a.riding").click(function() {
+		$("#twoRiding").toggle();
+	});
+	$("a.bmx").click(function() {
+		$("#threeBmx").toggle();
+	});
+	$("a.beach").click(function() {
+		$("#fourBeachcruiser").toggle();
+	});
 	$("img.magnify").mouseenter(function() {
-	 $(this).css({ width: '300', height: '315px' });
+	 	$(this).css({ width: '320', height: '300px' });
 	});
 	$("img.magnify").mouseleave(function() {
 		$(this).css({width: '200', height: '200'});
-	});
-	$("a.mountain").click(function() {
-		$("div").find("#oneMount").show('img');
-	});
-	$("a.riding").click(function() {
-		$("#twoRdidng.library").show();
-	});
-	$("a.bmx").click(function() {
-		$("#threeBmx").show();
-	});
-	$("a.beach").click(function() {
-		$("#fourBeachcruiser").show();
 	});
 	/*document.getElementById("mountain").onclick = function() {
 		$("div").find("#oneMount").show();
